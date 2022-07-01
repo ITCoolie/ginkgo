c1df124e7ed152d93be2faaeec4f743622c2bf94
unknown <yujian>
2022-07-01 20:05:45 +0800

    add ginkgo service

---
+++ web/service/src/test/java/com/ginkgo/service/ServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ginkgo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
